79ebda531a5310fa75c1135c6fbad43fa1fbc906
bakkermpd <merijn.debakker@tno.nl>
2021-12-03 16:48:01 +0100

    - work in progress issue #5

--- harlow/uncertainty_sampling.py
+++ harlow/uncertainty_sampling.py
@@ -40,7 +40,7 @@ class UncertaintySampler:
     def adaptive_surrogating(
         self, n_initial_point: int = None, n_new_point_per_iteration: int = 1
     ):
-        target_function = self.target_function
+        # target_function = self.target_function
         domain_lower_bound = self.domain_lower_bound
         domain_upper_bound = self.domain_upper_bound
         n_dim = len(domain_lower_bound)
@@ -49,39 +49,54 @@ class UncertaintySampler:
         if n_initial_point is None:
             n_initial_point = 5 * n_dim
 
-        if not target_function.is_probabilistic:
+        if not self.surrogate_model.is_probabilistic:
             raise NotImplementedError(
                 "Uncertainty based sampling only implemented for probabilistic \
                 surrogate models."
             )
 
-        if not self.fit_points_x:
+        if self.fit_points_x is None:
             points_x = latin_hypercube_sampling(
                 n_sample=n_initial_point,
                 domain_lower_bound=domain_lower_bound,
                 domain_upper_bound=domain_upper_bound,
             )
             points_y = self.target_function(points_x)
+        else:
+            points_x = self.fit_points_x
+            points_y = self.fit_points_y
 
         while convergence is False:
-            y_range = np.max(points_y) - np.min(points_y)
-            surrogate_model = self.surrogate_model()
-            surrogate_model.fit(self.fit_points_x, self.fit_points_y)
-            noise = surrogate_model.observation_noise_variance_var
+            y_range = np.max(points_y.flatten()) - np.min(points_y.flatten())
+            self.surrogate_model = self.surrogate_model()
+            self.surrogate_model.fit(points_x, points_y)
 
-            def prediction_std(x):
-                std = surrogate_model.predict(x)[1] - noise
-                return std
+            bounds = [
+                (domain_lower_bound[i], domain_upper_bound[i])
+                for i in range(len(domain_lower_bound))
+            ]
 
             diff_evolution_result = differential_evolution(
-                prediction_std, bounds=(domain_lower_bound, domain_upper_bound)
+                self.prediction_std, bounds=bounds
             )
             std_max = diff_evolution_result.fun
 
-            if std_max - noise <= self.epsilon * y_range:
+            if (
+                std_max - self.surrogate_model.observation_noise_variance_var
+                <= self.epsilon * y_range
+            ):
                 convergence = True
             else:
                 x_new = diff_evolution_result.x
                 y_new = self.target_function(x_new)
                 points_x = np.concatenate(points_x, x_new)
                 points_y = np.concatenate(points_y, y_new)
+
+    def prediction_std(self, x):
+
+        if x.ndim == 1:
+            x = np.reshape(x, (-1, 1))
+        # noise = self.surrogate_model.observation_noise_variance_var
+        noise = 0
+        std = self.surrogate_model.predict(x)[1] - noise
+        return std
