48711059f86bfc41f12d014f2d5d0108696f447c
Ioannis Koune <ioannis.koune@tno.nl>
2022-09-27 11:07:20 +0000

    docs: fix failing docs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,7 +69,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "_templates"]
+exclude_patterns = ["build", "_templates"]
 
 # bibliography settings
 bibtex_bibfiles = ["harlow_references.bib"]
@@ -89,7 +89,7 @@ html_theme = "furo"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["../_static"]
+html_static_path = ["./_static"]
 
 # this adds a custom javascript file that
 # * opens all external link in a new tab
