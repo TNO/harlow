271ba32b3e4631d2ab491279f8711b6e824fed9f
bakkermpd <merijn.debakker@tno.nl>
2022-11-30 14:24:09 +0100

    - adapted probabilistic sampler for new sampling loop

--- harlow/sampling/probabilistic_sampling.py
+++ harlow/sampling/probabilistic_sampling.py
@@ -56,12 +56,43 @@ class ProbabilisticSampler(Sampler):
             run_name,
             save_dir,
         )
-        surrogate = surrogate_model_constructor()
-        self.surrogate_models.append(surrogate)
-        # TODO: remove when sample is not being used anymore
-        self.surrogate_model = surrogate
+        # surrogate = surrogate_model_constructor()
+        # self.surrogate_models.append(surrogate)
+        # # TODO: remove when sample is not being used anymore
+        # self.surrogate_model = surrogate
 
         self.iterations = 0
+
+    def set_initial_set(self, points_x: np.ndarray, points_y: np.ndarray):
+        super().set_initial_set(points_x, points_y)
+        # Also create the output surrogates
+        for _i in range(self.dim_out):
+            self.surrogate_models.append(self.surrogate_model_constructor())
+
+    def _fit_models(self):
+        # Standard case assumes single model
+        for i, dim_surrogate_model in enumerate(self.surrogate_models):
+            dim_surrogate_model.fit(
+                self.fit_points_x, np.expand_dims(self.fit_points_y[:, i], axis=1)
+            )
+
+    def _update_models(
+        self, new_fit_points_x: np.ndarray, new_fit_points_y: np.ndarray
+    ):
+        # Standard case assumes single model
+        for i, dim_surrogate_model in enumerate(self.surrogate_models):
+            dim_surrogate_model.update(
+                new_fit_points_x, np.expand_dims(new_fit_points_y[:, i], axis=1)
+            )
+
+    def _predict(self):
+        # Standard case assumes single model
+        y = np.zeros((self.test_points_x.shape[0], self.dim_out))
+
+        for i, dim_surrogate_model in enumerate(self.surrogate_models):
+            a = dim_surrogate_model.predict(self.test_points_x)
+            y[:, i] = a[0]
+        return y
 
     def _best_new_points(self, n) -> np.ndarray:
         n_dim = len(self.domain_lower_bound)
@@ -93,7 +124,8 @@ class ProbabilisticSampler(Sampler):
 
         # TODO: how to select n points?
         x_new = np.expand_dims(diff_evolution_result.x, axis=0)
-        print(x_new)
+
+        return x_new
 
     def sample(
         self,
